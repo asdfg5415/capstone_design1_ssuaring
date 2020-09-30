b6357dd4bea2719ab9e6be50f97e195f11d80f5f
JJH <wnsgur8397@daum.net>
2020-09-30 19:31:57 +0900

    initial commit

---
+++ src/schema.js
@@ -0,0 +1,14 @@
+import path from "path"
+import {makeExecutableSchema} from "graphql-tools"
+import {fileLoader,mergeResolvers,mergeTypes}from "merge-graphql-schemas"
+console.log(__dirname);
+const allTypes = fileLoader(path.join(__dirname,"./api/**/*.graphql"))
+const allResolvers = fileLoader(
+    path.join(__dirname,"./api/**/*.js")
+)
+const typeDefs = mergeTypes(allTypes);
+const resolvers = mergeResolvers(allResolvers);
+const executableSchema = makeExecutableSchema({
+    typeDefs,resolvers
+})
+export default executableSchema;
