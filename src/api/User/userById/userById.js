630df886b92de901165402438152cb34248633ac
JJH <wnsgur8397@daum.net>
2020-10-12 16:42:35 +0900

    junzzi commit

---
+++ src/api/User/userById/userById.js
@@ -0,0 +1,11 @@
+import {prisma}from "../../../../generated/prisma-client"
+export default {
+
+  Query: {
+    userById: async(_, { id }) => {
+        return await prisma.user({
+            id:id
+        })
+    },
+  },
+};
