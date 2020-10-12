9169962a911b2246395275a7b7ee0f3d756f1c34
coolchaeyoung <ggodchae@naver.com>
2020-10-12 16:49:17 +0900

    me edit

---
+++ src/api/User/me/me.js
@@ -0,0 +1,10 @@
+
+export default {
+    Query: {
+        me: async(_, __, { request, isAuthenticated }) => {
+            isAuthenticated(request);
+            const { user } = request;
+            return await prisma.user({ id: user.id })
+        }
+    }
+};
