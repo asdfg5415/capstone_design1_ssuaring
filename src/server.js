60a0fba1b438d9c5512f0c3c48ce168e92ceea08
coolchaeyoung <ggodchae@naver.com>
2020-11-03 02:29:00 +0900

    passport

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 import './env';
 import { GraphQLServer } from 'graphql-yoga';
+import passport from "passport";
 import schema from './schema';
+import "./passport";
+import { authenticateJwt } from './passport';
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema }); //그래프큐엘서버 받아옴. props=schema ===> schema는 쿼리 뮤테이션, 리졸버들이 포함되있음
+const server = new GraphQLServer({ 
+  schema,
+  context: ({ request }) => ({ request })
+}); //그래프큐엘서버 받아옴. props=schema ===> schema는 쿼리 뮤테이션, 리졸버들이 포함되있음
+
+server.express.use(authenticateJwt);
+
 server.start({ port: PORT }, () =>
   console.log(`✅ server starting http://localhost:${PORT}`)
 ); //서버온
