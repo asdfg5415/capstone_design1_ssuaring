8aa019698b9a8d69a547b480b3c3cc4dee95e53c
koesnij <m_jinseok@naver.com>
2020-11-18 16:48:38 +0900

    upload #1

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import schema from './schema';
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema }); //그래프큐엘서버 받아옴. props=schema ===> schema는 쿼리 뮤테이션, 리졸버들이 포함되있음
+
 server.start({ port: PORT }, () =>
   console.log(`✅ server starting http://localhost:${PORT}`)
 ); //서버온
