be2b21108d3a1ab2094b44502026d433b07fadc5
JJH <wnsgur8397@daum.net>
2020-11-23 18:28:16 +0900

    search and profile

--- src/api/Post/searchPost/searchPost.js
+++ src/api/Post/searchPost/searchPost.js
@@ -4,11 +4,13 @@ export default {
   Query: {
     searchPost: async (_, args) => {
       const { term } = args;
+      console.log(term);
       const posts = await prisma.posts({
         where: {
           OR: [{ title_contains: term }, { caption_contains: term }],
         },
       });
+      console.log(posts);
       return posts;
     },
   },
