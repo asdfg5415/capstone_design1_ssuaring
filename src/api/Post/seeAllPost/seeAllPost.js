c93ea808409329bdc05d9f057ed08a0fe17f6046
Sanghyeon Shin <asdfg5415@gmail.com>
2020-11-28 18:08:33 +0900

    seeAllPost

---
+++ src/api/Post/seeAllPost/seeAllPost.js
@@ -0,0 +1,9 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        seeAllPost: async () => {
+            return await prisma.posts();
+        }
+    }
+}
