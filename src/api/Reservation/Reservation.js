51441899b0027cb67eb3b00c09e8a2fe8170c589
Sanghyeon Shin <asdfg5415@gmail.com>
2020-11-29 21:41:14 +0900

    Modify- Reservation Computed File

--- src/api/Reservation/Reservation.js
+++ src/api/Reservation/Reservation.js
@@ -4,5 +4,7 @@ export default {
   Reservation: {
     borrower: ({ id }) => prisma.reservation({ id }).borrower(),
     post: ({ id }) => prisma.reservation({ id }).post(),
+    startDate: ({ id }) => prisma.reservation({ id }).startDate(),
+    endDate: ({ id }) => prisma.reservation({ id }).endDate(),
   },
 };
