c7f0b8effbfd796628ce61cef876ea9f5bfcc68e
Sanghyeon Shin <asdfg5415@gmail.com>
2020-11-29 21:04:19 +0900

    delete see User dir For removing redundant

--- src/api/Reservation/reservationOfBorrower/reservationOfBorrower.js
+++ src/api/Reservation/reservationOfBorrower/reservationOfBorrower.js
@@ -2,7 +2,7 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
     Query: {
-        reservationByUserId: async(_, args) => {
+        reservationOfBorrower: async(_, args) => {
             const { id } = args;
             return prisma.reservations({ where : {
                 borrower : {
