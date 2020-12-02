ff584e8e4c49daca0863c649bf06ed0860eba454
koesnij <m_jinseok@naver.com>
2020-12-03 01:05:28 +0900

    isMine, isLiked, likeCount, reservationCount 필드 추가

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -1,9 +1,40 @@
-import { prisma } from "../../../generated/prisma-client";
+import { prisma } from '../../../generated/prisma-client';
 
 export default {
   Post: {
     files: ({ id }) => prisma.post({ id }).files(),
     user: ({ id }) => prisma.post({ id }).user(),
+    isMine: ({ id }, _, { request }) => {
+      const { user } = request;
+      return prisma
+        .post({ id })
+        .user()
+        .id()
+        .then(postId => postId === user.id);
+    },
+    isLiked: (parent, _, { request }) => {
+      const { user } = request;
+      return prisma.$exists.like({
+        AND: [
+          {
+            post: { id: parent.id },
+          },
+          {
+            user: { id: user.id },
+          },
+        ],
+      });
+    },
+    likeCount: ({ id }) =>
+      prisma
+        .likesConnection({ where: { post: { id } } })
+        .aggregate()
+        .count(),
     reservations: ({ id }) => prisma.post({ id }).reservations(),
+    reservationCount: ({ id }) =>
+      prisma
+        .reservationsConnection({ where: { post: { id } } })
+        .aggregate()
+        .count(),
   },
 };
